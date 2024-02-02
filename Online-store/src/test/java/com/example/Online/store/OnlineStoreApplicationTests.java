ad6c9e6532a58b4c583f6a3a79620ef913e4915e
nlls-05 <138480588+nlls-05@users.noreply.github.com>
2024-02-02 10:21:43 +0600

    first

---
+++ Online-store/src/test/java/com/example/Online/store/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Online.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
