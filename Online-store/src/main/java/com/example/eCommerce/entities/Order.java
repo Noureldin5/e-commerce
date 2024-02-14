8c89a4f46cf3ec0989b6e4a5daced79edb7867ef
nlls-05 <138480588+nlls-05@users.noreply.github.com>
2024-02-14 14:24:59 +0600

    ignore

--- Online-store/src/main/java/com/example/eCommerce/entities/Order.java
+++ Online-store/src/main/java/com/example/eCommerce/entities/Order.java
@@ -3,6 +3,10 @@ package com.example.eCommerce.entities;
 import java.util.Collection;
 
 public class Order {
+
+    private Long id;
+    private String serial_number;
+
     public Collection<Product> getProducts() {
         return null;
     }
