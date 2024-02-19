838c17e56d6ab1d4204d4e688fc2895d70af9b51
nlls-05 <138480588+nlls-05@users.noreply.github.com>
2024-02-19 15:16:08 +0600

    ignore

--- Online-store/src/main/java/com/example/eCommerce/service/order/Impl/OrderServiceImpl.java
+++ Online-store/src/main/java/com/example/eCommerce/service/order/Impl/OrderServiceImpl.java
@@ -1,4 +1,39 @@
 package com.example.eCommerce.service.order.Impl;
 
-public class OrderServiceImpl {
+import com.example.eCommerce.entities.Order;
+import com.example.eCommerce.repositories.OrderRepository;
+import com.example.eCommerce.repositories.UserRepository;
+import com.example.eCommerce.service.order.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+public class OrderServiceImpl implements OrderService {
+
+    @Override
+    public Page<Order> findByBuyerEmail(String name, PageRequest request) {
+        return null;
+    }
+
+    @Override
+    public Page<Order> findAll(PageRequest request) {
+        return null;
+    }
+
+    @Override
+    public Order findOne(Long orderId) {
+        return null;
+    }
+
+    @Override
+    public Order cancel(Long orderId) {
+        return null;
+    }
+
+    @Override
+    public Order finish(Long orderId) {
+        return null;
+    }
 }
